38f32ce7270ef6848a1fe1b8a207f3418d257eff
Ashraf Abubaker <ashraf.m.alraheem@gmail.com>
2018-12-07 11:28:34 +0200

    Working on the project and Modify the read me

--- main.c
+++ main.c
@@ -16,28 +16,31 @@
 /* Constants */
 #define DELAY1        (100000)
 #define DELAY2        (20000)
-#define MAX_SIZE      (25)
+#define MAX_SIZE      (24)
 
 
 /* Function Declaration */
 void blink_forever();
+// ** Global Vars **//
+uint8_t array1[MAX_SIZE];
+uint8_t array2[MAX_SIZE];
 /**
  * main.c
  */
 void main(void)
 {
-	uint8_t array1[MAX_SIZE];
-	uint8_t array2[MAX_SIZE];
+	uint32_t difference = array2-array1;
 	P1DIR |=BIT0;                                 // Set P1.0 as output pin
 	P2DIR |=BIT1;                                 // Set P2.1 as output pin
-    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watch dog timer
 	/* Set array 1 to AF */
-	my_memset(array1,MAX_SIZE,0xAF);
-	/* Make no operation for debuging */
+	my_memset(array1,MAX_SIZE,0xFF);
+	/* Make no operation for debugging */
 	__NOP();
 	/* Move array 1 to array 2 */
 	my_memmove(array1,array2,MAX_SIZE-1);
 	__NOP();
+	difference = 100;
 	/* Move array 1 to array 2 */
     blink_forever();
 }
@@ -48,6 +51,6 @@ void blink_forever()
     while(1){
         for(i=0;i<DELAY1;i++);
         P1OUT ^=BIT0;
-        P2OUT ^=BIT1;
+        //P2OUT ^=BIT1;
     }
 }
